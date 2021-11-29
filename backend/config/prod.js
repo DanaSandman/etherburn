3b5a9915eff6d6c0b4aead0343dcda68cf66a098
dana sandman <sandmandana1@gmail.com>
2021-11-29 18:39:42 +0200

    atlas

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // 'dbURL': 'mongodb+srv://artouradmin:artour123@cluster0.u2fhe.mongodb.net',
-  'dbURL': 'mongodb://localhost:27017',
+  'dbURL': 'mongodb+srv://etherburnadmin:etherburn123@cluster0.b7r16.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  // 'dbURL': 'mongodb://localhost:27017',
 }
