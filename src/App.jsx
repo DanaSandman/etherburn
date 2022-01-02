12f75f58f1053673b0d7502d28320d62eb5e3326
dana sandman <sandmandana1@gmail.com>
2022-01-02 20:26:52 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,10 @@ import { Collection } from "./pages/Collection.jsx";
 import { Header } from "./components/util/Header.jsx";
 import { Footer } from "./components/util/Footer.jsx";
 
-<<<<<<< HEAD
-// import Login from "./components/util/Login.jsx";
-// import Web3 from "web3";
-
-export function App() {
-  // const [isConnected, setIsConnected] = useState(false);
-  // const [currentAccount, setCurrentAccount] = useState(null);
-  // const [balance, setBalance] = useState(0);
-=======
 // import Web3 from "web3";
 
 export function App() {
   // const [currentAccount, setCurrentAccount] = useState(null);
->>>>>>> 594d23eefece9b0e44a3f4933b0e68b74d719f1d
 
   // const onLogin = async (provider) => {
   //   const web3 = new Web3(provider);
@@ -34,15 +24,7 @@ export function App() {
   //     const accBalanceEth = web3.utils.fromWei(
   //       await web3.eth.getBalance(accounts[0]),
   //       "ether"
-<<<<<<< HEAD
-  //     );
-
-  //     setBalance(Number(accBalanceEth).toFixed(6));
-  //     setIsConnected(true);
-  //   }
-=======
   //     )}
->>>>>>> 594d23eefece9b0e44a3f4933b0e68b74d719f1d
   // };
 
   return (
@@ -54,8 +36,6 @@ export function App() {
             <Route
               path="/"
               element={
-<<<<<<< HEAD
-=======
                 // <Home currentAccount={currentAccount} />
                 <Home />
               }
@@ -64,7 +44,6 @@ export function App() {
               path="/home"
               element={
                 // <Home currentAccount={currentAccount} />
->>>>>>> 594d23eefece9b0e44a3f4933b0e68b74d719f1d
                 <Home />
               }
             />
