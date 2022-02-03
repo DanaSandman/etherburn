79a065c8ec10e5e5ceeb995809205a06643ffcb5
dana sandman <sandmandana1@gmail.com>
2022-02-03 17:36:16 +0200

    update

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -2,10 +2,6 @@ import axios from 'axios'
 
 const BASE_URL = process.env.NODE_ENV === 'production' ? '/api/' : '//localhost:3030/api/'
 
-// var axios = axios.create({
-//     withCredentials: true
-// })
-
 export const httpService = {
     get(endpoint, data) {
         return ajax(endpoint, 'GET', data)
@@ -22,18 +18,13 @@ export const httpService = {
 }
 
 async function ajax(endpoint, method= 'GET', data=null) {
-    console.log('data',data);
-    // console.log('endpoint',endpoint);
-    console.log('method',method);
     try {
-        console.log(data);
         const res = await axios({
             url: `${BASE_URL}${endpoint}`,
             method,
             data,
             params: (method === 'GET') ? data : null
         })
-        console.log('res.data',res.data);
         return res.data
     } catch (err) {
         console.log(`Had Issues ${method}ing to the backend, endpoint: ${endpoint}, with data: ${data}`)
