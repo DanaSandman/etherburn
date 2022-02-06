83ec9da4c152af33fc75bd2e1afd3ba7bc7e462b
dana sandman <sandmandana1@gmail.com>
2022-02-06 15:50:37 +0200

    update text

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,8 +2,8 @@ import React, { Fragment } from "react";
 //assets
 import FullLogo from "../../assets/img/FullLogo.png";
 //FontAwesome
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faDiscord } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDiscord } from "@fortawesome/free-brands-svg-icons";
 
 export function Footer() {
   const socialPath = {
@@ -21,18 +21,20 @@ export function Footer() {
             className="footer-logo"
           ></img>
           <p>
-            Flype is a decentralized exchange for LP Tokes. Community driven
-            protocol governed by FLYP Stakers. Vote for key protocol parameters
-            and take part in the protocol's governance. Come and join our
-            growing community to learn more about our ongoing developments and
-            future plans, share your feedback and feature requests with us.
+            EtherBurn is an NFT collection that captures Ethereum burnt fee
+            metadata. Each EtherBurn NFT features a unique 3D design generated
+            by an algorithmic combination of timestamps, blocks and burnt fee
+            data. Once you’ve minted it, nobody else can get it.
           </p>
         </div>
         <div className="footer-social-media-links flex column">
           <h6>Follow Us</h6>
           <div className="social-links flex space-between">
             <a href={socialPath.linkedin} className="discord-footer-link">
-              <FontAwesomeIcon  className="discord-footer-link" icon={faDiscord} />
+              <FontAwesomeIcon
+                className="discord-footer-link"
+                icon={faDiscord}
+              />
             </a>
             <a
               href={socialPath.twitter}
@@ -45,10 +47,9 @@ export function Footer() {
               <i className="fa fa-instagram" />
             </a>
           </div>
-          <button className="join-our-discord-btn">           
-          <a href={socialPath.linkedin}>
-            JOIN OUR DISCORD
-          </a></button>
+          <button className="join-our-discord-btn">
+            <a href={socialPath.linkedin}>JOIN OUR DISCORD</a>
+          </button>
           <span>© 2021 EtherBurn. </span>
         </div>
       </div>
